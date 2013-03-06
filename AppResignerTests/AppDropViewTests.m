9bcbb0651d12886e16035314c8c12cd65ff293ae
Lonny Gomes <code@lonnygomes.com>
2013-03-06 08:27:50 -0500

    Added AppDropView tests. Still haven't been able to got OC mock properly working but I am commiting the start of this test.

---
+++ AppResignerTests/AppDropViewTests.m
@@ -0,0 +1,44 @@
+//
+//  AppDropViewTests.m
+//  AppResigner
+//
+//  Created by Carpe Lucem Media Group on 2/20/13.
+//  Copyright (c) 2013 Carpe Lucem Media Group. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+//#import "AppDropView.h"
+
+@interface AppDropViewTests : SenTestCase
+
+@end
+//@interface AppDropView(TestCases)
+//- (BOOL)isValidForFileAtPath:(NSString *)path;
+//- (NSString *)getFilenameFromPasteBoard:(id<NSDraggingInfo>)sender;
+//@property (nonatomic, assign) BOOL isValidFile;
+//@end
+
+@implementation AppDropViewTests
+
+//AppDropView *dropView;
+//
+//- (void)setUp
+//{
+//    [super setUp];
+//    
+//    dropView = [[AppDropView alloc] initWithFrame:NSRectFromCGRect(CGRectMake(0, 0, 100, 100))];
+//    
+//}
+//
+//- (void)testInit {
+//    STAssertFalse(dropView.isInDragState, @"Drag state should not be activated on init");
+//    STAssertFalse(dropView.isValidFile, @"There is no file, should be false");
+//}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+@end
